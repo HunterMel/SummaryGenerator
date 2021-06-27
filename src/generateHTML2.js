57019c96da4537e076254b5738ff8339346c74a0
Hunter Johnson <hunterjohnsoninfo@gmail.com>
2021-06-27 10:24:15 -0500

    edit cards

--- src/generateHTML2.js
+++ src/generateHTML2.js
@@ -3,22 +3,20 @@
 //const Manager = require("./lib/Manager")
 //const Employee = require("./lib/Employee")
 
-const generateCard = employee => {
-    //what type of employee 
-    const managerCard = manager => {
+const managerCard = manager => {
         return `
     <div class="card text-dark bg-info mb-3" style="max-width: 18rem;">
         <img src="http://tinygraphs.com/labs/isogrids/hexa" class="card-img-top" alt="avatar">
         <div class="card-body">
-            <h5 class="card-title">${employee.name}</h5>
-            <p class="card-text">${employee.role}</p>
+            <h5 class="card-title">${manager.name}</h5>
+            <p class="card-text">${manager.role}</p>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">${employee.id}</li>
-            <li class="list-group-item">${employee.email}</li>
+            <li class="list-group-item">${manager.id}</li>
+            <li class="list-group-item">${manager.email}</li>
         </ul>
         <div class="card-body">
-            <p class="card-link">Office Number: ${employee.officeNum}</p>
+            <p class="card-link">Office Number: ${manager.officeNum}</p>
         </div>
     </div>    
         `;
@@ -48,66 +46,54 @@ const generateCard = employee => {
     <div class="card text-dark bg-info mb-3" style="max-width: 18rem;">
         <img src="http://tinygraphs.com/labs/isogrids/hexa" class="card-img-top" alt="avatar">
         <div class="card-body">
-            <h5 class="card-title">${employee.name}</h5>
-            <p class="card-text">${employee.role}</p>
+            <h5 class="card-title">${intern.name}</h5>
+            <p class="card-text">${intern.role}</p>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">${employee.id}</li>
-            <li class="list-group-item">${employee.email}</li>
+            <li class="list-group-item">${intern.id}</li>
+            <li class="list-group-item">${intern.email}</li>
         </ul>
         <div class="card-body">
-            <p class="school"> School: <${employee.school}"></p>
+            <p class="school"> School: <${intern.school}"></p>
         </div>
     </div>    
         `;
-    }
 }
 
-
-
-    const generateHTML = employeesData => {
-            // call employee functions
-    if (role === 'Manager') {
-        const managerCard = generateManager(employee);
-
-        employeesData.push(managerCard);
-    }
-
-    // call engineer function
-    if (role === 'Engineer') {
-        const engineerCard = generateEngineer(employee);
-
-        employeesData.push(engineerCard);
-    }
-
-    // call intern function 
-    if (role === 'Intern') {
-        const internCard = generateIntern(employee);
-
-        employeesData.push(internCard);
-    }
-    
-        return `<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://code.jquery.com/jquery-3.6.0.slim.js" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-        <link rel="stylesheet" href="../style.css">
-    </head>
-    <body>
-        ${employeesData.map((employee) => {
-            return generateCard(employee)
-        })}
+const generateHTML = employeesData => { 
+    return `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+          <link rel="stylesheet" href="./src/style.css">
+          <title>Team Profiles</title>
+        </head>
+        <body>
+        
+        ${employeesData.map((employee) =>{
+          
+          switch(employee.getRole()) {
+            case 'Manager':
+             return generateManagerCard(employee)
+              break;
+            case 'Engineer':
+              return generateEngineerCard(employee)
+              break;
+            case 'Intern':
+              return generateInternCard(employee)
+              break;
+          }
+        }).join('')
+      }
         
         <script src="index.js"></script>
-    </body>
-</html>
-`
-    }
+        </body>
+      </html>
+    `;
+  }; 
 
 module.exports = generateHTML;
